fc82e6c3f41d15ff24ac2bc2057f11504cc3766e
Tanmay Sardesai <tanmay.sardesai3@gmail.com>
2016-11-17 19:01:26 -0600

    global.h is done

--- LED_Controller/include/global.h
+++ LED_Controller/include/global.h
@@ -16,21 +16,21 @@ using namespace std;
 
 typedef struct 
 {
-  GtkWidget *window1; //visualization window
+  GtkWidget *window1;  //!< pointer to the main window of GUI
 
-  GtkWidget *entry_sd; //the text field to contain the name of the serial device
-  GtkWidget *label_voltage; // the label on which we display the voltage
-GtkWidget *send_button;
-GtkWidget *open_button;
-GtkWidget *close_button;
-GtkWidget *exit_button;
-GtkWidget *entryR;
-GtkWidget *entryG;
-GtkWidget *entryB;
-GtkWidget *scaleR;
-GtkWidget *scaleG;
-GtkWidget *scaleB;
-GtkWidget *tx_label;
+  GtkWidget *entry_sd;  //!< pointer to the the text field to contain the name of the serial device of GUI
+  GtkWidget *label_voltage; //!< pointer to the the label on which we display the voltage of GUI
+GtkWidget *send_button; //!< pointer to the send button of the GUI
+GtkWidget *open_button; //!< pointer to the open button of the GUI
+GtkWidget *close_button; //!< pointer to the close button of the GUI
+GtkWidget *exit_button; //!< pointer to the exit button of the GUI
+GtkWidget *entryR; //!< pointer to the text box for entry for Red of the GUI
+GtkWidget *entryG; //!< pointer to the text box for entry for Green of the GUI
+GtkWidget *entryB; //!< pointer to the text box for entry for Bule of the GUI
+GtkWidget *scaleR; //!< pointer to the slider for Red of the GUI
+GtkWidget *scaleG; //!< pointer to the slider for Red of the GUI
+GtkWidget *scaleB; //!< pointer to the slider for Red of the GUI
+GtkWidget *tx_label; //!< pointer to the label to on which we display the RGB string sent.
 
   
 } Gui_Window_AppWidgets;
