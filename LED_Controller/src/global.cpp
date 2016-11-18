e349d233c1efcbe53fb771c2d8df0423d4c6091e
satej1210 <satej1210@live.com>
2016-11-17 19:06:23 -0600

    edited global.cpp

--- LED_Controller/src/global.cpp
+++ LED_Controller/src/global.cpp
@@ -7,15 +7,15 @@
 
 #include "global.h"
 
-Gui_Window_AppWidgets *gui_app; //!< structure to keep all interesting widgets
+Gui_Window_AppWidgets *gui_app; //! < structure to keep all interesting widgets
 
-int ser_dev=-1; //!< integer to store serial status
+int ser_dev=-1; //! < integer to store serial status
 
-char c_voltage_value[40]; //!< char array to store voltage value
+char c_voltage_value[40]; //! < char array to store voltage value
 
-int kill_all_threads; //!< bool integer to see if you need to kill threads.
+int kill_all_threads; //! < bool integer to see if you need to kill threads.
 
-GMutex *mutex_to_protect_voltage_display; //!< The mutex needed to keep voltage label in range
+GMutex *mutex_to_protect_voltage_display; //! < The mutex needed to keep voltage label in range
 
 
 
