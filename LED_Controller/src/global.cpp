1a5a78d1ed317a94a62526b4f710ebf9e91b49fe
satej1210 <satej1210@live.com>
2016-11-17 18:55:33 -0600

    Global.cpp done

--- LED_Controller/src/global.cpp
+++ LED_Controller/src/global.cpp
@@ -1,14 +1,28 @@
+/**
+ * \author Tanmay Sardesai
+ * \author Satej Mhatre
+ * \version 1.0
+ * \date 2016-17-15
+ * \bug None.
+ * \warning None.
+ *
+ * \copyright GNU Public License.
+ *
+ * \mainpage The Teensy LED Controller - Global.cpp
+ * \section intro_sec Introduction
+ */
+
 #include "global.h"
 
-Gui_Window_AppWidgets *gui_app; //structure to keep all interesting widgets
+Gui_Window_AppWidgets *gui_app; //!< structure to keep all interesting widgets
 
-int ser_dev=-1;
+int ser_dev=-1; //!< integer to store serial status
 
-char c_voltage_value[40];
+char c_voltage_value[40]; //!< char array to store voltage value
 
-int kill_all_threads;
+int kill_all_threads; //!< bool integer to see if you need to kill threads.
 
-GMutex *mutex_to_protect_voltage_display;
+GMutex *mutex_to_protect_voltage_display; //!< The mutex needed to keep voltage label in range
 
 
 
